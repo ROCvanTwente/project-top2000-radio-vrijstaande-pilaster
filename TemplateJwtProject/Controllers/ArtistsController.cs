3700f1ca5d99801bd9c11a092f9a8736c5971351
Dwastaken <d.winkelhorst456@gmail.com>
2026-01-15 11:27:20 +0100

    ArtistsApiController

--- TemplateJwtProject/Controllers/ArtistsController.cs
+++ TemplateJwtProject/Controllers/ArtistsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TemplateJwtProject.Data;
+using TemplateJwtProject.Models;
 
 namespace TemplateJwtProject.Controllers
 {
@@ -7,5 +10,34 @@ namespace TemplateJwtProject.Controllers
     [ApiController]
     public class ArtistsController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public ArtistsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Artists>>> GetArtists()
+        {
+            var artists = await _context.Artists.ToListAsync();
+            return Ok(artists);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Artists>> GetArtist(int id)
+        {
+            var artist = await _context.Artists.FindAsync(id);
+
+            if (artist == null)
+                return NotFound();
+
+            return Ok(artist);
+        }
+
+
+
     }
+
+
 }
---
+++ TemplateJwtProject/Controllers/Top2000EntriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateJwtProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Top2000EntriesController : ControllerBase
+    {
+    }
+}
